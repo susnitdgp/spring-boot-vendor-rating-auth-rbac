15e0659ce7242ca0439b25f8c21b0f1b02821c42
Susanta Goswami <susnitdgp@gmail.com>
2022-02-12 14:22:41 +0530

    Update VendorRatingApplication.java

--- src/main/java/in/co/ntpc/pradip/VendorRatingAuth/VendorRating/VendorRatingApplication.java
+++ src/main/java/in/co/ntpc/pradip/VendorRatingAuth/VendorRating/VendorRatingApplication.java
@@ -2,7 +2,12 @@ package in.co.ntpc.pradip.VendorRatingAuth.VendorRating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @SpringBootApplication
 public class VendorRatingApplication {
 
@@ -11,3 +16,4 @@ public class VendorRatingApplication {
 	}
 
 }
+
