f8969247b1def11011096b04758fa6784d6a0ea5
Susanta Goswami <susnitdgp@gmail.com>
2022-02-12 13:58:52 +0530

    Add files via upload

---
+++ src/main/java/in/co/ntpc/pradip/VendorRatingAuth/VendorRating/VendorRatingApplication.java
@@ -0,0 +1,13 @@
+package in.co.ntpc.pradip.VendorRatingAuth.VendorRating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorRatingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/co/ntpc/pradip/VendorRatingAuth/VendorRating/VendorRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package in.co.ntpc.pradip.VendorRatingAuth.VendorRating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorRatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
